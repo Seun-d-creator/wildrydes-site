786a0a6b220b087596d58f250f94fcd2f928f7dd
Seun-d-creator <manlikemo937@gmail.com>
2025-03-17 20:36:08 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_UFuyWRyRE', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '629nqu28niigqk295mb7d6081g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-1_8UqvDDH0x', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3q2rn1ik1lb9o7s0ps8p85cssj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
